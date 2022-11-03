90961a6a7606e5903313be66540dd917225f210f
Vitalii89vn <vitalii.kuhta@gmail.com>
2022-11-03 23:35:09 +0200

    task 1+2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,7 +4,7 @@ import { galleryItems } from './gallery-items.js';
 
 const gallery = document.querySelector('.gallery');
 const galleryLink = document.querySelector('.gallery__link');
-const galleryEl = document.querySelectorAll('.gallery__image');
+const modal = document.querySelectorAll('.modal');
 
 const markupGallery = galleryItems.map(({preview, original, description}) => { 
       return `
@@ -40,25 +40,26 @@ function onClickImg(event) {
 }
 
 document.querySelector('.gallery').onclick = () => {
-	basicLightbox.create(`
+  basicLightbox.create(`
+  <div class="modal">
 		<img width="1280" src="${originalImgUrl}">
+  </div>
 	`).show()
 
 }
-document.addEventListener('keydown', onClose)
- onClose: (instance) => {
+ onShow: (instance) => {
 	// Close when hitting escape.
 	document.onkeydown = function(evt) {
 		evt = evt || window.event;
-		var isEscape = false;
+		let isEscape = false;
 		if ( "key" in evt ) {
 			isEscape = ( evt.key === "Escape" || evt.key === "Esc" );
     }
-    // else {
-		// 	isEscape = ( evt.keyCode === 27 );
-		// }
+    else {
+			isEscape = ( evt.keyCode === 27 );
+		}
 		if ( isEscape ) {
 			instance.close();
 		}
 	};
-}
+ }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const galleryLink = document.querySelector('.gallery__link');
+const galleryEl = document.querySelectorAll('.gallery__image');
+
+const markupGallery = galleryItems.map(({ preview, original, description }) => {
+    return `
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+</a>
+    `
+}).join('');
+gallery.innerHTML = `${markupGallery}`;
+
+let lightbox = new SimpleLightbox('.gallery a');
+
+
+// gallery.addEventListener('click', onClickImg)
+// function onClickImg(event) {
+//     event.preventDefault();
+//     const currentEl = event.target;
+
+//     if (currentEl.nodeName !== 'IMG') {
+//         return
+//     }
+// }
